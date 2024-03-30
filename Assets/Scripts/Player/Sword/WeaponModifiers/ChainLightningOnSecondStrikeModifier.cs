823571dbec91d33b7164b890a64ec912fdea685b
Mario Gabriel Baravaglio <maritobaravaglio@gmail.com>
2024-03-30 02:05:13 -0300

    UI skill adjustment

---
+++ Assets/Scripts/Player/Sword/WeaponModifiers/ChainLightningOnSecondStrikeModifier.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "_ChainLightningOnSecondStrikeModifier", menuName = "WeaponModifiers/ChainLightningOnSecondStrikeModifier")]
+public class ChainLightningOnSecondStrikeModifier : WeaponModifierSO
+{
+    public override void ApplyModifier(PlayerController aPlayer)
+    {
+
+    }
+}
